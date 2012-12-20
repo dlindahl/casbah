5a07d3a005e62944e6cc486826ab1f2dbcb4cdde
Derek Lindahl <dlindahl@customink.com>
2012-12-20 12:32:59 -0500

    Adds saving and TGC conversion along with a full unit spec

--- app/models/ticket_granting_ticket.rb
+++ app/models/ticket_granting_ticket.rb
@@ -1,6 +1,8 @@
 class TicketGrantingTicket < Ticket
   attr_reader :username
 
+  validates_presence_of :username, allow_blank:false
+
   TicketNotFoundError = Class.new(StandardError)
 
   def initialize( params = {} )
@@ -9,11 +11,21 @@ class TicketGrantingTicket < Ticket
     @username = params[:username]
   end
 
+  def save
+    if valid?
+      redis.set id, @username
+    end
+  end
+
+  def to_tgc( request )
+    TicketGrantingCookie.new( self, request )
+  end
+
   class << self
     def find_by_tgc( tgc_id )
       return if tgc_id.blank?
 
-      tgt_id = tgc_id.gsub( %r{\ATGC-}, 'TGT-' )
+      tgt_id = tgc_id.gsub( %r{\ATGC-}, id_prefix )
 
       if username = redis.get( tgt_id )
         new id:tgt_id, username:username
@@ -24,3 +36,5 @@ class TicketGrantingTicket < Ticket
   end
 
 end
+
+TicketGrantingTicket.id_prefix = 'TGT-'
---
+++ spec/models/ticket_granting_ticket_spec.rb
@@ -0,0 +1,63 @@
+describe TicketGrantingTicket do
+  let(:id) { 'TGT-123' }
+
+  let(:username) { 'jdoe' }
+
+  let(:instance) { described_class.new( id:id, username:username ) }
+
+  describe '#save' do
+    subject { instance.save }
+
+    context 'with a valid ticket' do
+      it 'should save the ticket' do
+        subject
+
+        redis.get( id ).should eq username
+      end
+    end
+
+    context 'with an invalid ticket' do
+      let(:instance) { described_class.new }
+
+      it 'should not save the ticket' do
+        subject
+
+        redis.keys('*').should be_empty
+      end
+    end
+  end
+
+  describe '#to_tgc' do
+    let(:id) { 'TGT-123' }
+
+    let(:request) { double('request').as_null_object }
+
+    subject { instance.to_tgc( request ) }
+
+    it { should be_a TicketGrantingCookie }
+  end
+
+  describe '.find_by_tgc' do
+    let(:tgc) { 'TGC-123' }
+
+    subject { described_class.find_by_tgc( tgc ) }
+
+    context 'with a known value' do
+      before { Casbah.config.redis.set( id, username ) }
+
+      it { should be_a described_class }
+    end
+
+    context 'with an unknown value' do
+      it 'should raise an error' do
+        expect{ subject }.to raise_error TicketGrantingTicket::TicketNotFoundError
+      end
+    end
+
+    context 'with a nil value' do
+      let(:tgc) { nil }
+
+      it { should be_nil }
+    end
+  end
+end
